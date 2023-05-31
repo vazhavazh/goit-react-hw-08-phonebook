06084622d212de364dce65515ebad05bae34a9b5
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-05-31 23:40:14 +0300

    Update selectors.jsx

--- src/redux/contacts/selectors.jsx
+++ src/redux/contacts/selectors.jsx
@@ -7,9 +7,8 @@ export const selectFilter = state => state.contacts.filter;
 export const selectLoadingValue = state => state.contacts.contacts.isLoading;
 export const selectError = state => state.contacts.contacts.error;
 
-export const applayFilter = createSelector(
-  [selectContactsItems, selectFilter],
-  (contacts, filter) => {
+export const applayFilter = createSelector([selectContactsItems, selectFilter],
+(contacts, filter) => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
